35f91dd49e1bed69031b23400d48b0964697bcd4
MGSiqueira <marcos.siqueira98@gmail.com>
2022-05-02 16:38:49 -0300

    Pergunta 4 pronta

--- src/components/Question3.jsx
+++ src/components/Question3.jsx
@@ -2,7 +2,7 @@ export default function Question3({ handle }) {
   return (
     <div>
       <p>Gostaria de fazer parte da GRX?</p>
-      <select id={ 3 } onClick={ handle }>
+      <select id={ 'Pergunta3' } onClick={ handle }>
         <option value='Sim'>Sim</option>
         <option value='Não'>Não</option>
         <option value='Não sei'>Não sei</option>
